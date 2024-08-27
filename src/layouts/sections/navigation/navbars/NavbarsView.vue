42d97ddb2e636a1e2ca30f9b6e8025cd22eba79b
Aya Hosny <96696906+ayahosnii@users.noreply.github.com>
2024-08-27 22:20:34 +0300

    first commit

---
+++ src/layouts/sections/navigation/navbars/NavbarsView.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { onMounted } from "vue";
+
+// Sections components
+import BaseLayout from "../../components/BaseLayout.vue";
+import View from "../../components/View.vue";
+
+// Navbars page components
+import NavbarDark from "./components/NavbarDark.vue";
+
+// Navbars page components codes
+import { navbarDarkCode } from "./components/codes";
+
+
+</script>
+<template>
+  <BaseLayout
+    title="Navbars"
+    :breadcrumb="[{ label: 'Navigation', route: '#' }, { label: 'Navbars' }]"
+  >
+    <View
+      title="Navbar dark"
+      :code="navbarDarkCode"
+      id="navbar-dark"
+      height="300  position-relative"
+    >
+      <NavbarDark />
+    </View>
+  </BaseLayout>
+</template>
